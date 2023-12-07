52a6cc9189636e4f5993d2803a4087f3a9dfad96
MTariqR <5100155@mypillar7uwc.ac.za>
2023-12-07 21:24:26 +0200

    Cleaned up

--- SpokenNumber/NumberSpeak.cs
+++ SpokenNumber/NumberSpeak.cs
@@ -3,29 +3,114 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SpokenNumber
 {
     internal class NumberSpeak
     {
-        
-        /*public static string OneDigit(int num) 
+        public static string SingleDigit(int num)
         {
-            string word;
-            word = ;
-            return word;
+            string[] singleDigitArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+            return singleDigitArray[num];
         }
-        public string TwoDigit() 
+
+        public static string DoubleDigit(int num)
         {
-            return "";
+            string[] singleDigitArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+            string[] tenDigitArray = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+            string[] teensDigitArray = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+
+            string words ="";
+
+            if (num < 20)
+            {
+                num -= 10;
+                words += teensDigitArray[num];
+            }
+            else if (num % 10 == 0)
+            {
+                words += tenDigitArray[num / 10];
+            }
+            else
+            {
+                words += tenDigitArray[num / 10] + ' ' + singleDigitArray[num % 10];
+            }
+            return words;
         }
-        public string ThreeDigit() 
+
+        public static string TripleDigit(int num)
         {
-            return "";
+            string[] singleDigitArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+            string[] tenDigitArray = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+            string[] teensDigitArray = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+
+            string words = $"{singleDigitArray[num / 100]} Hundred";
+
+            if (num % 100 > 0 & num % 100 < 10)
+            {
+                words += " and " + singleDigitArray[num % 100];
+            }
+            else if ((num / 10) % 10 == 1)
+            {
+                words += " and " + teensDigitArray[num % 10];
+            }
+            else if ((num / 10) % 10 <= 9 & (num / 10) % 10 > 0)
+            {
+                words += $" and {tenDigitArray[(num / 10) % 10]} {singleDigitArray[num % 10]}";
+            }
+
+            return words;
         }
-        public string FourDigit() 
+
+        public static string FourDigit(int num)
         {
-            return "";
-        }*/
+            string[] singleDigitArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+            string[] tenDigitArray = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+            string[] teensDigitArray = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+
+            string words = $"{singleDigitArray[num / 1000]} Thousand";
+
+            num %= 1000;
+
+            if (num > 0 & num < 100)
+            {
+                if (num > 0 & num < 10)
+                {
+                    words += $" and {singleDigitArray[num]}";
+
+                }
+                else if (num < 20)
+                {
+                    num -= 10;
+                    words += $" and {teensDigitArray[num]}";
+                }
+                else if (num % 10 == 0)
+                {
+                    words += $" and {tenDigitArray[num / 10]}";
+                }
+                else
+                {
+                    words += $"{tenDigitArray[num / 10]} {singleDigitArray[num % 10]}";
+                }
+            }
+            else if (num >= 100 & num < 1000)
+            {
+                words += $" {singleDigitArray[num / 100]} Hundred ";
+                if (num % 100 > 0 & num % 100 < 10)
+                {
+                    words += $" and {singleDigitArray[num % 100]}";
+                }
+                else if ((num / 10) % 10 == 1)
+                {
+                    words += $" and {teensDigitArray[num % 10]}";
+                }
+                else if ((num / 10) % 10 <= 9 & (num / 10) % 10 > 0)
+                {
+                    words += $" and {tenDigitArray[(num / 10) % 10]} {singleDigitArray[num % 10]}";
+                }
+            }
+            return words;
+        }
     }
 }
--- SpokenNumber/Program.cs
+++ SpokenNumber/Program.cs
@@ -4,14 +4,9 @@
     {
         static void Main(string[] args)
         {
-            string[] singleDigitArray = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
-            string[] tenDigitArray = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
-            string[] teensDigitArray = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
-
             bool onOff = true;
-            string numberIn;
             Console.WriteLine("Please enter a number up to 4 digits.");
-            numberIn = Console.ReadLine();
+            string numberIn = Console.ReadLine();
 
             while (onOff)
             {
@@ -20,95 +15,28 @@
                     case 1:
                         {
                             int numCon = Convert.ToInt16(numberIn);
-                            Console.WriteLine(singleDigitArray[numCon]);
+                            Console.WriteLine(NumberSpeak.SingleDigit(numCon));
                             break;
                         }
 
                     case 2:
                         {
                             int numCon = Convert.ToInt16(numberIn);
-                            if (numCon < 20)
-                            {
-                                numCon -= 10;
-                                Console.WriteLine(teensDigitArray[numCon]);
-                            }
-                            else if (numCon % 10 == 0)
-                            {
-                                Console.WriteLine(tenDigitArray[numCon / 10]);
-                            }
-                            else
-                            {
-                                Console.WriteLine($"{tenDigitArray[numCon / 10]} {singleDigitArray[numCon % 10]}");
-                            }
+                            Console.WriteLine(NumberSpeak.DoubleDigit(numCon));
                             break;
                         }
 
                     case 3:
                         {
                             int numCon = Convert.ToInt16(numberIn);
-                            Console.Write($"{singleDigitArray[numCon / 100]} Hundred ");
-                            if (numCon % 100 > 0 & numCon % 100 < 10)
-                            {
-                                Console.WriteLine($"and {singleDigitArray[numCon % 100]}");
-                            }
-                            else if ((numCon / 10) % 10 == 1)
-                            {
-                                Console.WriteLine($"and {teensDigitArray[numCon % 10]}");
-                            }
-                            else if ((numCon / 10) % 10 <= 9 & (numCon / 10) % 10 > 0)
-                            {
-                                Console.WriteLine($"and {tenDigitArray[(numCon / 10) % 10]} {singleDigitArray[numCon % 10]}");
-                            }
-                            else
-                                Console.WriteLine();
-
+                            Console.WriteLine(NumberSpeak.TripleDigit(numCon));
                             break;
                         }
 
                     case 4:
                         {
                             int numCon = Convert.ToInt16(numberIn);
-                            Console.Write($"{singleDigitArray[numCon / 1000]} Thousand ");
-                            numCon %= 1000;
-
-                            if (numCon > 0 & numCon < 100)
-                            {
-                                if (numCon>0 &numCon<10)
-                                {
-                                    Console.WriteLine($"and {singleDigitArray[numCon]}");
-
-                                }
-                                else if (numCon < 20)
-                                {
-                                    numCon -= 10;
-                                    Console.WriteLine($"and {teensDigitArray[numCon]}");
-                                }
-                                else if (numCon % 10 == 0)
-                                {
-                                    Console.WriteLine(tenDigitArray[numCon / 10]);
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"{tenDigitArray[numCon / 10]} {singleDigitArray[numCon % 10]}");
-                                }
-                            }
-                            else if (numCon>=100 & numCon < 1000)
-                            {
-                                Console.Write($"{singleDigitArray[numCon / 100]} Hundred ");
-                                if (numCon % 100 > 0 & numCon % 100 < 10)
-                                {
-                                    Console.WriteLine($"and {singleDigitArray[numCon % 100]}");
-                                }
-                                else if ((numCon / 10) % 10 == 1)
-                                {
-                                    Console.WriteLine($"and {teensDigitArray[numCon % 10]}");
-                                }
-                                else if ((numCon / 10) % 10 <= 9 & (numCon / 10) % 10 > 0)
-                                {
-                                    Console.WriteLine($"and {tenDigitArray[(numCon / 10) % 10]} {singleDigitArray[numCon % 10]}");
-                                }
-                            }
-                            Console.WriteLine();
+                            Console.WriteLine(NumberSpeak.FourDigit(numCon));
                             break;
                         }
 
